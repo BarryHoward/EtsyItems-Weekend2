2300d5af11ad8a99546422cb39c26a1782a1ac41
Barry Howard <BarryLeeHoward@gmail.com>
2016-10-09 03:10:45 -0400

    Finished assignment most likely

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-// Function that prints out a list of parameters to the console
-
-function printOut(array, parameter){
-	array.forEach(function(element){
-		if (!Array.isArray(parameter)){
-			console.log(element[parameter])
-		} else {
-			parameter.forEach(function(parElement){
-				console.log(element[parElement]);
-			})
-		}
-	});
-}
+//Functions
+//-----------------------------------------------
 
 // Reduce function to sum price
 function sumPrice(previousValue, currentValue, index, array){
@@ -50,8 +39,7 @@ function addParam(cusArray, orgArray, parameter){
 	return newArray;
 }
 
-//Function that filters Arrays which contain a parameter that matches a value when the values of the parameter are also an array
-
+// Function that filters Arrays which contain a parameter that matches a value when the values of the parameter are also an array
 function filterArrayValue(array, parameter, value){
 	var newArray = array.filter(function(element){
 		if (!Array.isArray(element[parameter])){
@@ -67,6 +55,7 @@ function filterArrayValue(array, parameter, value){
 	return newArray;
 }
 
+// Function that filters array based on length of a parameter
 function filterArrayLength(array, parameter, testLength){
 	var newArray = array.filter(function(element){
 		return (element[parameter].length >= testLength);
@@ -74,36 +63,136 @@ function filterArrayLength(array, parameter, testLength){
 	return newArray;
 }
 
+//Function that reads an array, and prints Param1 + some text + Param2 to a parent element
+function writeHTML (array, param1, text, param2, parent){
+	if (!Array.isArray(array)){
+		if (param1 === null && param2 === null){
+			var tempP = text;
+		} else if (param1 === null){
+			var tempP = text + array[param2];
+		} else if (param2 === null){
+			var tempP = array[param1] + text;
+		} else if (Array.isArray(array[param2])){
+			var tempP = array[param1] + text + array[param2].length + " " + param2 + ":";
+		} else {
+			var tempP = array[param1] + text + array[param2];
+		}
+		var paragraph = document.createElement("p");
+		paragraph.innerHTML = tempP
+		parent.appendChild(paragraph);
+		if (param2 !== null){
+			if (Array.isArray(array[param2])){
+				writeArray(array[param2], parent);
+			}
+		}
+	} else {
+		array.forEach(function(element){
+			if (param1 === null && param2 === null){
+				var tempP = text;
+			} else if (param1 === null){
+				var tempP = text + element[param2];
+			} else if (param2 === null){
+				var tempP = element[param1] + text;
+			} else if (Array.isArray(element[param2])){
+				var tempP = element[param1] + text + element[param2].length + " " + param2 + ":";
+			} else {
+				var tempP = element[param1] + text + element[param2];
+			}
+			var paragraph = document.createElement("p");
+			var pBreak = document.createElement("br");
+			paragraph.innerHTML = tempP;
+			parent.appendChild(paragraph);
+			if (param2 !== null){
+				if (Array.isArray(element[param2])){
+					writeArray(element[param2], parent);
+				}
+			}
+			parent.appendChild(pBreak);
+		})
+	}
+}
+
+function writeArray (paramArray, parent){
+	paramArray.forEach(function(element){
+		var paragraph = document.createElement("p");
+		paragraph.innerHTML = element;
+		parent.appendChild(paragraph);
+	})
+}
+
+// HTML ID lookup
+//-----------------------------------------
+
+P1 = document.getElementById("answer1");
+P2 = document.getElementById("answer2");
+P3 = document.getElementById("answer3");
+P4 = document.getElementById("answer4");
+P5 = document.getElementById("answer5");
+P6 = document.getElementById("answer6");
 
 
-
-
+//Calculations
+//------------------------------------------
 
 // Problem 1
 priceSum = items.reduce(sumPrice, 0);
-priceAvg = priceSum/items.length;
-console.log("Avg Price is " + priceAvg);
+priceAvg = Math.round(priceSum/items.length*100)/100;
 
 // Problem 2
 rangeItems = items.filter(priceRange);
-console.log("Items with prices between 14 and 18 are ");
-printOut(rangeItems, "title");
+rangeTitles = paramArray(rangeItems, "title");
 
 // Problem 3
-console.log("Items with currency code 'GBP' are ");
 var curArray = filterArrayValue(items, "currency_code", "GBP");
-printOut(curArray, ["title", "price"]);
 
 // Problem 4
 var matArray = filterArrayValue(items, "materials", "wood");
-console.log("The wood array is ");
-printOut(matArray, ["title", "price", "materials"]);
 
 // Problem 5
-lengthArray = filterArrayLength(items, "materials", 8);
-console.log("Items with 8 or more materials ");
-printOut(lengthArray, "title");
+var lengthArray = filterArrayLength(items, "materials", 8);
 
 // Problem 6
 var sameMade = filterArrayValue(items, "who_made", "i_did");
-console.log(sameMade.length);
+
+
+
+//Write to HTML
+//--------------------------------------------
+
+writeHTML(null, null, "The average price is $" + priceAvg, null, P1);
+writeHTML(rangeTitles, "title", "", null, P2);
+writeHTML(curArray, "title", " costs $", "price", P3);
+writeHTML(matArray, "title", " is made of wood", null, P4);
+writeHTML(lengthArray, "title", " has ", "materials", P5);
+writeHTML(null, null, sameMade.length + " were made by their sellers", null, P6);
+
+
+
+
+
+
+
+// ADDITIONAL FUNCTIONS for debugging
+// ---------------------
+
+// Function that prints out a list of parameters to the console
+function printOut(array, parameter){
+	array.forEach(function(element){
+		if (!Array.isArray(parameter)){
+			console.log(element[parameter])
+		} else {
+			parameter.forEach(function(parElement){
+				console.log(element[parElement]);
+			})
+		}
+	});
+}
+
+function objToArray(obj){
+	var newArray =[];
+	for (i = 0; i<Object.keys(obj).length; i++){
+		newArray[i] = obj[Object.keys(obj)[i]];
+
+	}
+	return newArray;
+}
